76680801b11fbea54e7ad6ab988bbd1b7fc7a9ee
Chymaster <xcheng@protonmail.com>
2021-09-03 13:14:07 +0000

    Update for class

--- Monte_Carlo_Lib.py
+++ Monte_Carlo_Lib.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #Hello
+Class Particles:
+    def __init__(x,y,identity):
+        #self.p for position
+        self.p = np.array((x,y)
+        self.identity = identity
+
