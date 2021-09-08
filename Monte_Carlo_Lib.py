60c45e905daf4476ccfc7184efbae7e806e4dc29
Chymaster <xcheng@protonmail.com>
2021-09-08 11:49:08 +0000

    restructred, only left Lattice class

--- Monte_Carlo_Lib.py
+++ Monte_Carlo_Lib.py
@@ -1,28 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #Hello
-class Particle:
-    def __init__(self,x,y,identity="a"):
-        #self.p for position
-        self.p = np.array((x,y))
-        self.identity = identity
-
 class Lattice:
-    def __init__(self,nx):
-        # Lattice.grid generates an array of size nx by nx
+    def __init__(self,nx,rho,fa):
+        #nx is for  Lattice.grid generates an array of size nx by nx
+        #rho is the density of particles 
+        #fa is fraction of particles that's type "A"
+
+        #Generating empty grid
         self.grid = np.zeros((nx,nx),str)
-        self.particles = []
+        self.grid_size = nx
 
-    def type(self,x,y):
-        return self.grid[x][y]
-
-    def add(self,x,y,identity):
-        if self.grid[x][y] = "0":
-            self.grid[x][y] = identity
-            self.particles.append(Particle(x,y,identity))
-
-    def swap(self,x,y,target_x,target_y):
-        self.grid[x][y]
-        
-
+        #Now filling the particles in
+        all_particles = np.full(int(rho*(nx**2)),'A')
+        #Filled up with A,
+        #Now turn some of them into B
+        for i in range(len(all_particles)):
+            if np.random.random > rho:
+                all_particles[i] = "B"
+    
+    def energy(self,eps_aa,eps_ab,eps_bb)
 
