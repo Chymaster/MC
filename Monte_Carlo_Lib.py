e8144b16b9f25de7eeb0c7f80b123454479354a9
Chymaster <xcheng@protonmail.com>
2021-09-08 10:38:02 +0000

    new ideas

--- Monte_Carlo_Lib.py
+++ Monte_Carlo_Lib.py
@@ -11,25 +11,17 @@ class Latice:
     def __init__(self,nx):
         # Lattice.grid generates an array of size nx by nx
         self.grid = np.zeros((nx,nx),str)
-
-class Sim:
-    #nx is for size of the grid
-    def __init__(self,nx,rho,fa):
-        self.size = nx
-        self.density = rho
-        self.fraction = fa
-
-    def particle_gen(self):
-        self.grid = Lattice.grid(nx)
         self.particles = []
-        #self.na/na is number of particles "a" and "b"  in this simulation
-        self.na = self.size^2 * fa
-        self.nb = self.size^2 * (1-fa)
+
+    def type(self,x,y):
+        return self.grid[x][y]
+
+    def add(self,x,y,identity):
+        if self.grid[x][y] = "0":
+            self.grid[x][y] = identity
+            self.particles.append(Particle(x,y,identity))
+
+    def swap(self):
         
-        #assign random position to particles
-        while len(self.particles) < (self.na+self.nb):
-            random_p =[ np.random.randint(0,nx),np.random.randint(0,nx)]
-            if self.grid[random_p[0][random_p[1]] != "0":
-                    #in grid, a "0" means no occupation, "1" means particle occupied but not yet assigned identity
-                    self.particles.append(Particle(*random_p,"1"))
-        print(self.grid)
+
+
