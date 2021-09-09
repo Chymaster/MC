f17ed652bb2fad5cc4e3bf4968c2eb18af6fe0cb
Chymaster <xcheng@protonmail.com>
2021-09-09 14:25:09 +0200

    Added plot for the grid

--- Monte_Carlo_Lib.py
+++ Monte_Carlo_Lib.py
@@ -1,25 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 class Lattice:
     def __init__(self, nx, rho, fa):
         #nx is for  Lattice.grid generates an array of size nx by nx
         #rho is the density of particles 
-        #fa is fraction of particles that's type "A"
+        #fa is fraction of particles that's type 1 for A
 
         #Generating empty grid
-        self.grid = np.zeros((nx,nx),str)
+        self.grid = np.zeros((nx,nx),int)
         self.grid_size = nx
         ptc_n = int(rho*(nx**2)) #number of particles
 
         #Generate a list of particles
-        all_particles = np.full(ptc_n,'A')
+        all_particles = np.full(ptc_n,1)
         #Filled up with A,
         #Now turn some of them into B
         for i in range(ptc_n):
             if np.random.random() > fa:
-                all_particles[i] = "B"
+                all_particles[i] = 2
         all_particles = iter(all_particles)
 
         #Putting the particles in grid
@@ -32,28 +31,34 @@ class Lattice:
 
 
     def map(self):
-        print(self.grid)
-        #plt.imshow(self.grid)
-        #Doesn't work bc self.grid is str array
-        #plt.show()
+        import matplotlib.patches as mpatches
+        import matplotlib.pyplot as plt
+
+        im = plt.imshow(self.grid)
+        colors = [im.cmap(im.norm(value)) for value in [0,1,2]]
+        patches = [mpatches.Patch(color=colors[0], label="Empty".format(l=0)), mpatches.Patch(color=colors[1], label="Particle A".format(l=1)),mpatches.Patch(color=colors[2], label="Particle B".format(l=2))]
+
+        plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+        plt.show()
     
     def energy(self,eps_aa,eps_ab,eps_bb):
         energy = 0
         for i in range(len(self.grid)-1):
             for j in range(len(self.grid[0])-1):
-                if self.grid[i][j] == "A":
+                if self.grid[i][j] == 1:
                     #Energy if the vertex pair start with type A
-                    if self.grid[i][j+1]=="A" or self.grid[i+1][j]=="A":
+                    if self.grid[i][j+1]==1 or self.grid[i+1][j]== 1:
                         #aa pair for (vx,vy+1) and (vx+1,vy)
                         energy += eps_aa
-                    elif self.grid[i][j+1]=="B" or self.grid[i+1][j]=="B":
+                    elif self.grid[i][j+1]==2 or self.grid[i+1][j]==2:
                         #ab pair
                         energy += eps_ab
-                elif self.grid[i][j] == "B":
-                    if self.grid[i][j+1]=="A" or self.grid[i+1][j]=="A":
+                elif self.grid[i][j] == 2:
+                    if self.grid[i][j+1]==1 or self.grid[i+1][j]==1:
                         #ba pair
                         energy += eps_ab
-                    elif self.grid[i][j+1]=="B" or self.grid[i+1][j]=="B":
+                    elif self.grid[i][j+1]==2 or self.grid[i+1][j]==2:
                         #bb pair
                         energy += eps_bb
         return energy
