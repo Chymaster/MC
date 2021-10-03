c986686a40c2938483dc7b575c8ff758c24c166f
Chymaster <xcheng@protonmail.com>
2021-10-03 15:25:03 +0100

    Some style change

--- Monte_Carlo_Lib.py
+++ Monte_Carlo_Lib.py
@@ -2,52 +2,41 @@ import numpy as np
 
 
 class Lattice:
-    def __init__(self, nx, rho, fa, eps = (1,1,1)):
+    def __init__(self, nx, rho, fa):
         # nx is for  Lattice.grid generates an array of size nx by nx
         # rho is the density of particles
         # fa is fraction of particles that's type 1 for A
         # eps is for energy per verdex pair, the order is (eps_aa,eps,ab,eps_ab)
 
         # Generating empty grid
-        self.grid = np.zeros((nx, nx), int)
         self.grid_size = nx
-        self.eps = eps
-        ptc_n = int(rho * (nx ** 2))  # number of particles
+
 
         # Generate a list of particles
-        all_particles = np.full(ptc_n, 1)
+        particle_n = int(rho * (nx ** 2))  # number of particles
+        all_particles = np.full(particle_n, 1)
         # Filled up with A,
         # Now turn some of them into B
-        for i in range(ptc_n):
+        for i in range(particle_n):
             if np.random.random() > fa:
                 all_particles[i] = 2
         all_particles = iter(all_particles)
 
-        # Putting the particles in grid
+        # Grid with random particle configuration
+        grid = np.zeros((nx, nx), int)
         all_positions = np.indices((nx, nx)).reshape(2, -1).T  # a pool of positions in index [x,y]
         np.random.shuffle(all_positions)  # shuffle the pool
-        for i in all_positions[
-                 :ptc_n]:  # loop in the pool until all particle are filled ([:ptc_n] only keeps the first ptc_n number
-            self.grid[i[0]][i[1]] = next(all_particles)
+        for i in all_positions[:particle_n]:  # loop in the pool until all particle are filled ([:particle_n] only keeps the first particle_n number
+            grid[i[0]][i[1]] = next(all_particles)
 
-    def map(self):
-        import matplotlib.patches as mpatches
-        import matplotlib.pyplot as plt
+        self.grid = grid
 
-        im = plt.imshow(self.grid)
-        colors = [im.cmap(im.norm(value)) for value in [0, 1, 2]]
-        patches = [mpatches.Patch(color=colors[0], label="Empty".format(l=0)),
-                   mpatches.Patch(color=colors[1], label="Particle A".format(l=1)),
-                   mpatches.Patch(color=colors[2], label="Particle B".format(l=2))]
-        plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-
-        plt.show()
 
     def energy(self, eps_aa, eps_ab, eps_bb):
         energy = 0
 
         # A check position function is added for ease of periodic boundary conditions
-        def check_position(x,y):
+        def check_position(x, y):
             if x < 0:
                 x = self.grid_size - x
             if y < 0:
@@ -64,14 +53,14 @@ class Lattice:
             for j in range(len(self.grid[0])):
                 if check_position(i,j) == 1:
                     # Energy if the vertex pair start with type A
-                    if check_position(i,j + 1) == 1 or check_position(i + 1,j) == 1:
+                    if check_position(i, j+1) == 1 or check_position(i+1, j) == 1:
                         # aa pair for (vx,vy+1) and (vx+1,vy)
                         energy += eps_aa
-                    elif check_position(i,j + 1) == 2 or check_position(i + 1,j) == 2:
+                    elif check_position(i, j+1) == 2 or check_position(i+1, j) == 2:
                         # ab pair
                         energy += eps_ab
-                elif check_position(i,j) == 2:
-                    if check_position(i,j + 1) == 1 or check_position(i + 1,j) == 1:
+                elif check_position(i, j) == 2:
+                    if check_position(i,j+1) == 1 or check_position(i+1, j) == 1:
                         # ba pair
                         energy += eps_ab
                     elif check_position(i,j + 1) == 2 or check_position(i + 1,j) == 2:
@@ -115,3 +104,18 @@ class Lattice:
     def accept(self):
 
         return True
+
+
+
+    def map(self):
+        import matplotlib.patches as mpatches
+        import matplotlib.pyplot as plt
+
+        im = plt.imshow(self.grid)
+        colors = [im.cmap(im.norm(value)) for value in [0, 1, 2]]
+        patches = [mpatches.Patch(color=colors[0], label="Empty".format(l=0)),
+                   mpatches.Patch(color=colors[1], label="Particle A".format(l=1)),
+                   mpatches.Patch(color=colors[2], label="Particle B".format(l=2))]
+        plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+        plt.show()
